ba035adeddffe7a84397a5878e0aa30628b441c2
ri-dev-git <rishi9850@outlook.com>
2023-06-01 16:28:50 +0530

    fix response

--- packages/frontend/src/pages/auth/login.tsx
+++ packages/frontend/src/pages/auth/login.tsx
@@ -13,30 +13,27 @@ export default function login() {
 
   const { data: session, status } = useSession()
   const breakpoints = {
-    sm: '30em', // 480px
-    md: '48em', // 768px
-    lg: '62em', // 992px
-    xl: '80em', // 1280px
-    '2xl': '96em', // 1536px
+    sm: '280px',
   }
-
+  
+   extendTheme({ breakpoints })
   return (
 
     <Box display="flex" direction="row" w="100%" height="100%" alignItems="center" justifyContent="center">
       <Flex direction="column" w="50%" border="0px" h="100%" alignItems="center" justifyContent="center" width={["100%"]}>
         <Box  w={["80%","60%"]}h="45%" border="0px">
           <Box h="10%"/>
-          <Box display="flex" lineHeight="32px" alignItems="center" fontSize={["26px","24px","24px","26px"]} h="15%">
+          <Box display="flex" lineHeight="32px" alignItems="center" fontSize={["20px","26px","26px","24px","22px","26px"]} h="15%">
             <Text>Art</Text><Text color="#FE3796">Place</Text>
           </Box>
           <Box lineHeight="39px"h="15%" fontWeight="600">
-            <Text fontSize={["28px","30px","32px","32px"]} >Log In To Continue</Text>
+            <Text fontSize={["22px","28px","35px","30px","32px"]} >Log In To Continue</Text>
           </Box>
-          <Box fontSize="16px"lineHeight="28px" fontWeight="400">
+          <Box fontSize={["16px","16px","25px","14px","16px"]} lineHeight="28px" fontWeight="400">
             <Text>Sign a message using any of your wallet to login</Text>
           </Box>
-          <Box h="12%"/>
-          <Box height={["12%","20%"]}  display="flex" alignItems="center" justifyContent="center">
+          <Box h={["12%","12%","12%","11%","11%"]}/>
+          <Box height={["12%","18%"]}  display="flex" alignItems="center" justifyContent="center">
           <ConnectButton.Custom>
       {({
         account,
@@ -68,7 +65,7 @@ export default function login() {
             {(() => {
               if (!connected) {
                 return (
-                  <Button onClick={openConnectModal} borderRadius="6px" width="100%" height="100%" background="#FE3796" >
+                  <Button onClick={openConnectModal} borderRadius="6px" fontSize={["12px","18px","","16px"]} width="100%" height="100%" background="#FE3796" >
                   Click Here Sign In
                 </Button>
                 );
@@ -126,7 +123,7 @@ export default function login() {
       }}
     </ConnectButton.Custom>
           </Box>
-          <Box h="13%"/>
+          <Box h={["12%","12%","12%","11%","11%"]}/>
           <Box display="flex" alignItems="center" justifyContent="center">
             <Text color="#4B5563" fontWeight="600">New User?</Text><Text fontWeight="600" color="#FE3796">Register Now</Text>
           </Box>
@@ -136,26 +133,26 @@ export default function login() {
 
 
         <Flex magin="0" padding="0" direction="column" alignItems="center" justifyContent="center" background= "linear-gradient(180deg, #F461A7 0%, #6D0034 100%)"display={{ base: "none", lg: "block" }} width={[ "0%","100%"]} h="100%">
-          <Flex direction="row" justifyContent="center" alignItems="end" width="100%" height="65%"  >
-            <Flex justifyContent="center" alignItems="center" width="90%" border="0px" margin="0">
-            <Flex  justifyContent="space-between" border="0px" alignItems="center"  direction="column" >
+          <Flex direction="row" justifyContent="center" alignItems="end" width="100%" height={["0","0","0","67%","65%"]}  >
+            <Flex h="100%"justifyContent="center" alignItems="end" width="90%" border="0px" margin="0">
+            <Flex h="80.5%"justifyContent="center"  border="0px"alignItems="center"  direction="column" >
               <Image src={nft2} width={196.21} height={196.21}/>
-             
+              <Box h="1%"/>
               <Image src={nft3} width={196.21} height={196.21}/>
             </Flex>
             <Box width="1.5%"/>
           
-          <Flex h="100%"justifyContent="center" alignItems="center" direction="row">
+          <Flex h="80.5%" justifyContent="center" alignItems="center" direction="row">
               <Image src={nft1} alt="nft1" width={396.21} height={396.21}  />            
           </Flex>
           </Flex>
           </Flex>
           <Flex direction="column" width="100%" height="25%" alignItems="center" justifyContent="center">
           
-              <Heading w="70%" size="sm" color="rgba(255, 255, 255, 0.6)" fontWeight="600" fontSize="14px"lineHeight="28px" >
+              <Heading w={["","","","80%","70%"]} size="sm" color="rgba(255, 255, 255, 0.6)" fontWeight="600" fontSize="14px"lineHeight="28px" >
                   NFT FACTS
               </Heading>
-              <Text size="3xl" w="70%"fontWeight="400" lineHeight="30px" >
+              <Text size={["","","","18px","14px","20px"]} w={["","","","80%","70%"]} fontWeight="400" lineHeight="30px" >
                 NFTs (Non-Fungible Tokens) have been used to sell digital art for millions of dollars, with the highest price paid being over $69 million for a piece by digital artist Beeple.
               </Text>
           </Flex >
