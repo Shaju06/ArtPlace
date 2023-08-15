298fb01be18f9fd106ae467adcbe9738e367919c
Varinder <Varipaul@gmail.com>
2023-08-15 10:56:37 +0530

    un commnented nft test cases

--- packages/contracts/test/NftMarketPlace.test.ts
+++ packages/contracts/test/NftMarketPlace.test.ts
@@ -1,39 +1,39 @@
-// import { expect } from "chai";
-// import  { ethers } from "hardhat";
+import { expect } from "chai";
+import  { ethers } from "hardhat";
 
-// describe("NFTMarketplace", function () {
-//   let NFTMarketplace;
-//   let nftMarketplace;
-//   let NFT, Token;
-//   let nft, token;
-//   let owner, user1, user2;
+describe("NFTMarketplace", function () {
+  let NFTMarketplace;
+  let nftMarketplace;
+  let NFT, Token;
+  let nft, token;
+  let owner, user1, user2;
 
-//   before(async function () {
-//     [owner, user1, user2] = await ethers.getSigners();
+  before(async function () {
+    [owner, user1, user2] = await ethers.getSigners();
 
-//     NFT = await ethers.getContractFactory("NFT"); // Assuming you have an NFT contract
-//     nft = await NFT.deploy("MyNFT", "NFT");
+    NFT = await ethers.getContractFactory("NFT"); // Assuming you have an NFT contract
+    nft = await NFT.deploy("MyNFT", "NFT");
 
-//     Token = await ethers.getContractFactory("Token"); // Assuming you have a Token contract
-//     token = await Token.deploy();
+    Token = await ethers.getContractFactory("Token"); // Assuming you have a Token contract
+    token = await Token.deploy();
 
-//     NFTMarketplace = await ethers.getContractFactory("NFTMarketplace");
-//     nftMarketplace = await NFTMarketplace.deploy(nft.address, token.address);
-//   });
+    NFTMarketplace = await ethers.getContractFactory("NFTMarketplace");
+    nftMarketplace = await NFTMarketplace.deploy(nft.address, token.address);
+  });
 
-//   it("should list and buy NFTs", async function () {
-//     // Mint an NFT for user1
-//     await nft.connect(owner).mint(user1.address, 1);
+  it("should list and buy NFTs", async function () {
+    // Mint an NFT for user1
+    await nft.connect(owner).mint(user1.address, 1);
 
-//     // List NFT by user1
-//     await nft.connect(user1).approve(nftMarketplace.address, 1);
-//     await nftMarketplace.connect(user1).listNFT(1, ethers.utils.parseEther("1.0"));
+    // List NFT by user1
+    await nft.connect(user1).approve(nftMarketplace.address, 1);
+    await nftMarketplace.connect(user1).listNFT(1, ethers.utils.parseEther("1.0"));
 
-//     // Buy NFT by user2
-//     await token.connect(user2).approve(nftMarketplace.address, ethers.utils.parseEther("1.1"));
-//     await nftMarketplace.connect(user2).buyNFT(1);
+    // Buy NFT by user2
+    await token.connect(user2).approve(nftMarketplace.address, ethers.utils.parseEther("1.1"));
+    await nftMarketplace.connect(user2).buyNFT(1);
 
-//     const newOwner = await nft.ownerOf(1);
-//     expect(newOwner).to.equal(user2.address);
-//   });
-// });
+    const newOwner = await nft.ownerOf(1);
+    expect(newOwner).to.equal(user2.address);
+  });
+});
